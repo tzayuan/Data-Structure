b513a61e81e4641b156117b57ecf4de01d37e9ab
snowball <729756854@qq.com>
2019-10-05 10:13:54 +0800

    Select Sort

--- D_S/Linear/Sort.h
+++ D_S/Linear/Sort.h
@@ -3,6 +3,13 @@
 #include <cstdio>
 #include <cstdlib>
 
+//稳定性:定义：假定在待排序的记录序列中，存在多个具有相同的关键字的记录，
+//	若经过排序，这些记录的相对次序保持不变，即在原序列中，r[i]=r[j]，且r[i]在r[j]之前，
+//		而在排序后的序列中，r[i]仍在r[j]之前，则称这种排序算法是稳定的；
+//	否则称为不稳定的。
+//稳定的排序：直接插入排序、冒泡排序、归并排序
+//不稳定的排序：希尔排序、直接选择排序、堆排序、快速排序
+
 typedef int KeyType;
 typedef char Sort_ElemType[10];
 typedef struct{
@@ -14,7 +21,17 @@ InSort_SqList* InitRandomSqList();//这里写的不好,用了static,对于结构体数组作为形
 
 void DispInitKey(InSort_SqList *R);
 
+//内排序算法
+	//基于交换思想的比较排序算法
 void Bubble_Sort(InSort_SqList *R, int length);
 void Quick_Sort(InSort_SqList *R, int s, int t);
+	//基于插入思想的比较排序算法
 void Insert_Sort(InSort_SqList *R, int length);
 void Shell_Sort(InSort_SqList *R, int length);
+	//基于选择思想的比较排序算法
+void Select_Sort(InSort_SqList *R, int length);
+void Sift(InSort_SqList *R, int low, int high);
+void Heap_Sort(InSort_SqList *R, int length);
+	//归并排序算法
+
+	//基数排序算法
